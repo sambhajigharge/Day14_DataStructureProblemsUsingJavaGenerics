881395068be022afc77d50b2188faf237c5a2fbe
sushant <sgharge299@gmail.com>
2022-07-09 20:38:41 +0530

    [sushant]created the add first method

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -1,22 +1,19 @@
 package DataStructure.LinkedList;
 
+import java.util.Scanner;
+
 public class LinkedListMain {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to LinkedList Data Structure Program");
-        Node firstNode = new Node(56);
-        Node secondNode = new Node(30);
-        Node thirdNode = new Node(70);
-        Node head = firstNode;
-        firstNode.next = secondNode;
-        secondNode.next = thirdNode;
-        Node tail = thirdNode;
-
-        Node temp = head;
-        while (temp != null) {
-            System.out.println(temp.data);
-            temp = temp.next;
+        System.out.println("Welcome to linkedList DataStructure Program");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1 to add the data at the start");
+        switch (scanner.nextInt()) {
+            case 1:
+                Operations.addDataAtStart();
         }
+
     }
 }
+
 
