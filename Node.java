e5495a5f638d02cf8a68ebd5ef8a80e72c12740f
sushant <sgharge299@gmail.com>
2022-07-09 19:16:01 +0530

    [Sushant]Started the Data structure using the java generics program with welcome message

---
+++ Node.java
@@ -0,0 +1,7 @@
+package DataStructure.LinkedList;
+
+public class Node {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Linked List Data Structure Program");
+    }
+}
